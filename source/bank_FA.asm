f267c16690b937ca16d186ec76548748550f2e16
Mattrizzle <m.grassman87@gmail.com>
2023-03-10 07:41:48 -0500

    Moved ship mast tiledata into exhi bank 42
    This is to fix an issue created by the previous commit.  Since the boss icon tiledata grew by $1000 larger, everything else was shifted by that amount, resulting in the last few frames of the ship mast animated flag tiledata being overwritten by DATA_BA8000.

--- source/bank_FA.asm
+++ source/bank_FA.asm
@@ -97,37 +97,5 @@ gameover_screen_8x8_tilemap:
 DATA_FA5C41:
 	incbin "data/screens/8x8_tilemaps/gameover_screen_text_layer_1_8x8_tilemap.bin"
 
-;$FA5FC1
-DATA_FA5FC1:
-	incbin "data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame1.bin"
-
-;$FA6381
-DATA_FA6381:
-	incbin "data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame2.bin"
-
-;$FA6741
-DATA_FA6741:
-	incbin "data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame3.bin"
-
-;$FA6B01
-DATA_FA6B01:
-	incbin "data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame4.bin"
-
-;$FA6EC1
-DATA_FA6EC1:
-	incbin "data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame5.bin"
-
-;$FA7281
-DATA_FA7281:
-	incbin "data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame6.bin"
-
-;$FA7641
-DATA_FA7641:
-	incbin "data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame7.bin"
-
-;$FA7A01
-DATA_FA7A01:
-	incbin "data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame8.bin"
-
 padbyte $00
 pad $FB0000
--- source/exhi/bank_42.asm
+++ source/exhi/bank_42.asm
@@ -2,3 +2,34 @@
 ;pad $420040
 ;incsrc "data/physics_data.bin"
 
+;$FA5FC1
+DATA_FA5FC1:
+	incbin "../data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame1.bin"
+
+;$FA6381
+DATA_FA6381:
+	incbin "../data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame2.bin"
+
+;$FA6741
+DATA_FA6741:
+	incbin "../data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame3.bin"
+
+;$FA6B01
+DATA_FA6B01:
+	incbin "../data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame4.bin"
+
+;$FA6EC1
+DATA_FA6EC1:
+	incbin "../data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame5.bin"
+
+;$FA7281
+DATA_FA7281:
+	incbin "../data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame6.bin"
+
+;$FA7641
+DATA_FA7641:
+	incbin "../data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame7.bin"
+
+;$FA7A01
+DATA_FA7A01:
+	incbin "../data/backgrounds/animated_graphics/ship_mast_level_flag_tiledata_frame8.bin"
